b36b6e6391dd492414e9ed6c3e37554364ee0d95
Jochum Sloot <jochum.sloot@gmail.com>
2021-07-13 10:41:09 +0200

    chnaged the main.yml with digital ocean doctl

---
+++ .bash_aliases
@@ -0,0 +1,1 @@
+alias dir=".deployers.sh"
---
+++ deployer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "welcome"
+ls
+echo "this is the whole list of dir"
+mkdir textfiles
+cd textfiles 
+touch some_file.txt
+nano some_file.txt
+echo "Hello world!"
+exit
+git pull
+systemctl restart Jochum_sloot-CD 
+
+
